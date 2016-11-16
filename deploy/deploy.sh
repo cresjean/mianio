6a8209966cf17ecaa97c16c306541cda5eb02a4e
Crespo Wang <crespo.wang@servian.com.au>
2016-11-16 12:18:45 +1100

    deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ssh-agent bash -c 'ssh-add /var/tmp/github.secret; git clone git@github.com:cresjean/mianio.git'
-
-sleep 1000
+aws s3 cp /mianio/s3 s3://mianio.com --recursive
+echo "Done"
