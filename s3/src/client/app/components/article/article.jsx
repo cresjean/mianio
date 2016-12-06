01b48857342f8dd5c17f2ba3e8a769f21a4157f1
Crespo Wang <crespo.wang@servian.com.au>
2016-12-07 10:02:59 +1100

    and in moment

--- s3/src/client/app/components/article/article.jsx
+++ s3/src/client/app/components/article/article.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Grid, Col, Row } from 'react-bootstrap';
 import { render } from 'react-dom';
+import moment from 'moment';
+
 require ('./article.scss');
 class Article extends React.Component {
   constructor(props) {
@@ -15,6 +17,17 @@ class Article extends React.Component {
               <Row className="showGrid">
                 <Col xs={12}>
                   <i className="fa fa-tags" aria-hidden="true"></i>
+                  {
+                    this.props.article.tags.map((object, index) => {
+                      if (index == this.props.article.tags.length - 1) {
+                        return <span key={index}>{object.name}</span>
+                      } else {
+                        return (
+                          <span key={index}> {object.name} | </span>
+                        )
+                      }
+                    })
+                  }
                 </Col>
               </Row>
               <Row className="showGrid">
@@ -24,7 +37,16 @@ class Article extends React.Component {
                   </h1>
                 </Col>
               </Row>
-
+              <Row className="showGrid">
+                <Col xs={12}>
+                    {moment(this.props.article.created * 1000).format('D MMM')}
+                  </Col>
+              </Row>
+              <Row className="showGrid">
+                <Col xs={12}>
+                    {this.props.article.abstract}
+                  </Col>
+              </Row>
             </Grid>
 
 
